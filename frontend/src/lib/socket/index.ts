93fff0dfb48e14351c4cb29a70a1f019e3210ed5
Jacobtread <jacobtread@gmail.com>
2023-05-03 13:39:10 +1200

    Replaced socket with function based system that restarts itself, added prettier formatting

--- frontend/src/lib/socket/index.ts
+++ frontend/src/lib/socket/index.ts
@@ -1,111 +1,173 @@
 import { writable } from "svelte/store";
 import { DEBUG } from "../constants";
-import { ServerMessageType, type JoinedMessage, type OtherPlayerMessage, type GameStateMessage, type TimeSyncMessage, type QuestionMessage, type ScoresMessage, type ErrorMessage, type KickedMessage, type ServerMessage, type ClientMessage } from "./models";
+import {
+  ServerMessage,
+  type JoinedMessage,
+  type OtherPlayerMessage,
+  type GameStateMessage,
+  type TimeSyncMessage,
+  type QuestionMessage,
+  type ScoresMessage,
+  type ErrorMessage,
+  type KickedMessage,
+  type ServerMessageBody,
+  type ClientMessage,
+} from "./models";
 
+type MessageHandler<T> = (msg: T) => void;
+type MessageHandlers = {
+  [T in ServerMessage]: MessageHandler<ServerMessageBody<T>>;
+};
 
+// Reference to the socket
+let socket: WebSocket = createSocket();
+
+// Map of the message types to their handlers
+const messageHandlers: MessageHandlers = {
+  [ServerMessage.Joined]: onJoined,
+  [ServerMessage.OtherPlayer]: onOtherPlayer,
+  [ServerMessage.GameState]: onGameState,
+  [ServerMessage.TimeSync]: onTimeSync,
+  [ServerMessage.Question]: onQuestion,
+  [ServerMessage.Scores]: onScores,
+  [ServerMessage.Error]: onError,
+  [ServerMessage.Kicked]: onKicked,
+};
+
+// Socket readiness state
+export const socketReady = writable<boolean>(false);
+
+/**
+ * Creates a new socket connection assigning
+ * the event handlers for the different events
+ */
+function createSocket(): WebSocket {
+  // Create the socket
+  const ws = new WebSocket(getSocketURL());
+
+  // Assign the message handler
+  ws.onmessage = onMessage;
+
+  // Handle open events
+  ws.onopen = () => {
+    // Handle the WebSocket connection becoming OPEN
+    if (ws.readyState == WebSocket.OPEN) {
+      socketReady.set(true);
+    }
+  };
+
+  // Handle close events
+  ws.onclose = (event: CloseEvent) => {
+    // Handle the socket becoming unavailable
+    socketReady.set(false);
+    console.warn("WebSocket connetion closed", event);
+
+    // Reconnect the socket
+    socket = createSocket();
+  };
+
+  // Handle error events
+  ws.onerror = (event: Event) => {
+    // Handle the socket becoming unavailable
+    socketReady.set(false);
+    console.error("WebSocket error", event);
+
+    // Reconnect the socket
+    socket = createSocket();
+  };
+
+  return ws;
+}
+
+/**
+ * Obtains a URL to the endpoint for connecting the WebSocket.
+ * For debug mode this is a constant value otherwise the website
+ * origin is used
+ *
+ * @returns The URL that the WebSocket should use
+ */
 function getSocketURL(): URL {
-    const SOCKET_ENDPOINT: string = "/api/quiz/socket";
+  const SOCKET_ENDPOINT: string = "/api/quiz/socket";
 
-    if (DEBUG) {
-        return new URL(SOCKET_ENDPOINT, "ws://localhost");
-    }
+  let host = DEBUG
+    ? "ws://localhost"
+    : window.location.origin.replace(/^https/, "wss").replace(/^http/, "ws");
 
-    let host = window.location.origin
-        .replace(/^https/, 'wss')
-        .replace(/^http/, 'ws')
-
-    return new URL(SOCKET_ENDPOINT, host);
+  return new URL(SOCKET_ENDPOINT, host);
 }
 
-const socketStore = writable<QuizSocket | null>(null);
+/**
+ * Sends the provided message to the server through
+ * the socket
+ *
+ * @param msg
+ */
+export function sendMessage(msg: ClientMessage) {
+  console.debug("Sending message to server", msg);
 
-
-
-type PacketHandlers = {
-    [T in ServerMessageType]: (msg: ServerMessage<T>) => void
+  const data = JSON.stringify(msg);
+  try {
+    socket.send(data);
+  } catch (e) {
+    console.error("Failed to send message", e);
+  }
 }
 
+/**
+ * Event handler for parsing, and handling messages
+ * recieved from the server
+ *
+ * @param data The message data from the event
+ */
+function onMessage<T extends ServerMessage>({ data }: MessageEvent) {
+  // Parse the message
+  const msg: { ty: T | undefined } & ServerMessageBody<T> = JSON.parse(data);
 
+  // Ensure the message type is specified
+  if (msg.ty === undefined) {
+    console.error("Packet missing message type", data);
+    return;
+  }
 
+  // Find the handler for the message
+  const handler = messageHandlers[msg.ty];
+  if (handler === undefined) {
+    console.error("Handler not defined for packet type", msg.ty);
+    return;
+  }
 
+  // Call the handler with the mesasge
+  handler.call(socket, msg);
+}
 
-// WebSocket wrapper that establishes and handles a connection
-// to the Quiz logic
-class QuizSocket {
-    // The WebSocket connection
-    private ws: WebSocket;
+function onJoined(msg: JoinedMessage) {
+  console.debug("Joined message", msg);
+}
 
-    // Defines the respective functions that will handle each message
-    // type from the server
-    private handlers: PacketHandlers = {
-        [ServerMessageType.Joined]: this.handleJoined.bind(this),
-        [ServerMessageType.OtherPlayer]: this.handleOtherPlayer.bind(this),
-        [ServerMessageType.GameState]: this.handleGameState.bind(this),
-        [ServerMessageType.TimeSync]: this.handleTimeSync.bind(this),
-        [ServerMessageType.Question]: this.handleQuestion.bind(this),
-        [ServerMessageType.Scores]: this.handleScores.bind(this),
-        [ServerMessageType.Error]: this.handleError.bind(this),
-        [ServerMessageType.Kicked]: this.handleKicked.bind(this),
-    }
+function onOtherPlayer(msg: OtherPlayerMessage) {
+  console.debug("Other player message", msg);
+}
 
-    constructor() {
-        const ws = new WebSocket(getSocketURL())
-        ws.onmessage = this.onMessage.bind(this);
-        this.ws = ws;
-    }
+function onGameState(msg: GameStateMessage) {
+  console.debug("Game state message", msg);
+}
 
-    send(msg: ClientMessage) {
-        const data = JSON.stringify(msg);
-        this.ws.send(data);
-    }
+function onTimeSync(msg: TimeSyncMessage) {
+  console.debug("Time sync message", msg);
+}
 
-    onMessage<T extends ServerMessageType>(event: MessageEvent) {
-        console.log(this);
-        console.log(event);
+function onQuestion(msg: QuestionMessage) {
+  console.debug("Question message", msg);
+}
 
+function onScores(msg: ScoresMessage) {
+  console.debug("Score message", msg);
+}
 
-        const data: string = event.data;
-        const msg: { ty: T } & ServerMessage<T> = JSON.parse(data);
+function onError(msg: ErrorMessage) {
+  console.error("Server error", msg.error);
+}
 
-        console.log(msg);
-
-        const handler = this.handlers[msg.ty];
-
-        if (handler) {
-            handler(msg);
-        }
-
-    }
-
-    handleJoined(msg: JoinedMessage) {
-
-    }
-
-    handleOtherPlayer(msg: OtherPlayerMessage) {
-
-    }
-
-    handleGameState(msg: GameStateMessage) {
-
-    }
-
-    handleTimeSync(msg: TimeSyncMessage) {
-
-    }
-
-    handleQuestion(msg: QuestionMessage) {
-
-    }
-
-    handleScores(msg: ScoresMessage) {
-
-    }
-
-    handleError(msg: ErrorMessage) {
-
-    }
-
-    handleKicked(msg: KickedMessage) {
-
-    }
+function onKicked(msg: KickedMessage) {
+  console.debug("Kick message", msg);
 }
--- frontend/src/lib/socket/models.ts
+++ frontend/src/lib/socket/models.ts
@@ -1,223 +1,228 @@
 export interface CreatedResponse {
-    uuid: string;
+  uuid: string;
 }
 
 export type SessionId = number;
 export type GameToken = string;
 
 export interface JoinedMessage {
-    id: SessionId;
-    token: GameToken;
-    config: GameConfig;
+  id: SessionId;
+  token: GameToken;
+  config: GameConfig;
 }
 
 export interface OtherPlayerMessage {
-    id: SessionId;
-    name: string;
+  id: SessionId;
+  name: string;
 }
 
 export interface GameStateMessage {
-    state: GameState;
+  state: GameState;
 }
 
 export interface TimeSyncMessage {
-    total: number;
-    elapsed: number;
+  total: number;
+  elapsed: number;
 }
 
 export interface QuestionMessage {
-    question: Question;
+  question: Question;
 }
 
 export interface ScoresMessage {
-    scores: Record<SessionId, number>;
+  scores: Record<SessionId, number>;
 }
 
 export interface ErrorMessage {
-    error: ServerError;
+  error: ServerError;
 }
 
 export interface KickedMessage {
-    session_id: SessionId,
-    reason: RemoveReason;
+  session_id: SessionId;
+  reason: RemoveReason;
 }
 
-export const enum ServerMessageType {
-    Joined = "Joined",
-    OtherPlayer = "OtherPlayer",
-    GameState = "GameState",
-    TimeSync = "TimeSync",
-    Question = "Question",
-    Scores = "Scores",
-    Error = "Error",
-    Kicked = "Kicked",
+export const enum ServerMessage {
+  Joined = "Joined",
+  OtherPlayer = "OtherPlayer",
+  GameState = "GameState",
+  TimeSync = "TimeSync",
+  Question = "Question",
+  Scores = "Scores",
+  Error = "Error",
+  Kicked = "Kicked",
 }
 
-
-
-export type ServerMessage<T> =
-    T extends ServerMessageType.Joined ? JoinedMessage :
-    T extends ServerMessageType.OtherPlayer ? OtherPlayerMessage :
-    T extends ServerMessageType.GameState ? GameStateMessage :
-    T extends ServerMessageType.TimeSync ? TimeSyncMessage :
-    T extends ServerMessageType.Question ? QuestionMessage :
-    T extends ServerMessageType.Scores ? ScoresMessage :
-    T extends ServerMessageType.Error ? ErrorMessage :
-    T extends ServerMessageType.Kicked ? KickedMessage : unknown;
-
+// Transforms the provided ServerMessage variant into the associated
+// message content for that message
+export type ServerMessageBody<T> = T extends ServerMessage.Joined
+  ? JoinedMessage
+  : T extends ServerMessage.OtherPlayer
+  ? OtherPlayerMessage
+  : T extends ServerMessage.GameState
+  ? GameStateMessage
+  : T extends ServerMessage.TimeSync
+  ? TimeSyncMessage
+  : T extends ServerMessage.Question
+  ? QuestionMessage
+  : T extends ServerMessage.Scores
+  ? ScoresMessage
+  : T extends ServerMessage.Error
+  ? ErrorMessage
+  : T extends ServerMessage.Kicked
+  ? KickedMessage
+  : unknown;
 
 export interface InitializeMessage {
-    uuid: string;
+  uuid: string;
 }
 
 export interface ConnectMessage {
-    token: GameToken;
+  token: GameToken;
 }
 
 export interface JoinMessage {
-    name: string;
+  name: string;
 }
 
 export interface HostActionMessage {
-    action: HostAction;
+  action: HostAction;
 }
 
 export interface AnswerMessage {
-    answer: Answer;
+  answer: Answer;
 }
 
 export interface KickMessage {
-    id: SessionId;
+  id: SessionId;
 }
 
 export const enum ClientMessageType {
-    Initialize = "Initialize",
-    Connect = "Connect",
-    Join = "Join",
-    Ready = "Ready",
-    HostAction = "HostAction",
-    Answer = "Answer",
-    Kick = "Kick",
+  Initialize = "Initialize",
+  Connect = "Connect",
+  Join = "Join",
+  Ready = "Ready",
+  HostAction = "HostAction",
+  Answer = "Answer",
+  Kick = "Kick",
 }
 
-export type ClientMessage = (
-    ({ ty: ClientMessageType.Initialize } & InitializeMessage) |
-    ({ ty: ClientMessageType.Connect } & ConnectMessage) |
-    ({ ty: ClientMessageType.Join } & JoinMessage) |
-    ({ ty: ClientMessageType.Ready }) |
-    ({ ty: ClientMessageType.HostAction } & HostActionMessage) |
-    ({ ty: ClientMessageType.Answer } & AnswerMessage) |
-    ({ ty: ClientMessageType.Kick } & KickMessage)
-);
-
+export type ClientMessage =
+  | ({ ty: ClientMessageType.Initialize } & InitializeMessage)
+  | ({ ty: ClientMessageType.Connect } & ConnectMessage)
+  | ({ ty: ClientMessageType.Join } & JoinMessage)
+  | { ty: ClientMessageType.Ready }
+  | ({ ty: ClientMessageType.HostAction } & HostActionMessage)
+  | ({ ty: ClientMessageType.Answer } & AnswerMessage)
+  | ({ ty: ClientMessageType.Kick } & KickMessage);
 
 export interface GameConfig {
-    basic: BasicConfig;
+  basic: BasicConfig;
 }
 
 export interface BasicConfig {
-    name: string;
-    text: string;
+  name: string;
+  text: string;
 }
 
 export const enum GameState {
-    Lobby = 0x0,
-    Starting = 0x1,
-    AwaitingReady = 0x2,
-    AwaitingAnswers = 0x3,
-    Marked = 0x4,
-    Finished = 0x5,
+  Lobby = 0x0,
+  Starting = 0x1,
+  AwaitingReady = 0x2,
+  AwaitingAnswers = 0x3,
+  Marked = 0x4,
+  Finished = 0x5,
 }
 
 export const enum ServerError {
-    MalformedMessage = 0x0,
-    InvalidToken = 0x1,
-    UsernameTaken = 0x2,
-    NotJoinable = 0x3,
-    UnknownPlayer = 0x4,
-    Unexpected = 0x5,
-    InvalidPermission = 0x6,
-    UnexpectedMessage = 0x7,
-    AlreadyAnswered = 0x8,
-    InvalidAnswer = 0x9,
+  MalformedMessage = 0x0,
+  InvalidToken = 0x1,
+  UsernameTaken = 0x2,
+  NotJoinable = 0x3,
+  UnknownPlayer = 0x4,
+  Unexpected = 0x5,
+  InvalidPermission = 0x6,
+  UnexpectedMessage = 0x7,
+  AlreadyAnswered = 0x8,
+  InvalidAnswer = 0x9,
 }
 
 export const enum HostAction {
-    Start = 0x1,
-    Cancel = 0x2,
-    Skip = 0x3,
+  Start = 0x1,
+  Cancel = 0x2,
+  Skip = 0x3,
 }
 
 export const enum RemoveReason {
-    RemovedByHost = 0x1,
-    HostDisconnect = 0x2,
-    LostConnection = 0x3,
-    Disconnected = 0x4,
+  RemovedByHost = 0x1,
+  HostDisconnect = 0x2,
+  LostConnection = 0x3,
+  Disconnected = 0x4,
 }
 
 type ImageRef = string;
 type QuestionIndex = number;
 
 interface SingleQuestionData {
-    values: string[];
+  values: string[];
 }
 
 export interface MultipleQuestionData {
-    values: string[];
-    min: number | null;
-    max: number | null;
+  values: string[];
+  min: number | null;
+  max: number | null;
 }
 
 export interface ClickableImageQuestionData {
-    imag: ImageRef;
+  imag: ImageRef;
 }
 
 export const enum QuestionDataType {
-    Single = "Single",
-    Multiple = "Multiple",
-    ClickableImage = "ClickableImage"
+  Single = "Single",
+  Multiple = "Multiple",
+  ClickableImage = "ClickableImage",
 }
 
 export interface QuestionBase {
-    title: string;
-    text: string;
-    image: ImageRef | null;
-    answer_time: number;
-    scoring: Scoring;
+  title: string;
+  text: string;
+  image: ImageRef | null;
+  answer_time: number;
+  scoring: Scoring;
 }
 
-export type Question = QuestionBase & (
-    { ty: QuestionDataType.Single } & SingleQuestionData |
-    { ty: QuestionDataType.Multiple } & MultipleQuestionData |
-    { ty: QuestionDataType.ClickableImage } & ClickableImageQuestionData
-)
+export type Question = QuestionBase &
+  (
+    | ({ ty: QuestionDataType.Single } & SingleQuestionData)
+    | ({ ty: QuestionDataType.Multiple } & MultipleQuestionData)
+    | ({ ty: QuestionDataType.ClickableImage } & ClickableImageQuestionData)
+  );
 
 export interface Scoring {
-    min_score: number;
-    max_score: number;
-    bonus_score: number;
+  min_score: number;
+  max_score: number;
+  bonus_score: number;
 }
 
 export const enum AnswerType {
-    Single = "Single",
-    Multiple = "Multiple",
-    ClickableImage = "ClickableImage"
+  Single = "Single",
+  Multiple = "Multiple",
+  ClickableImage = "ClickableImage",
 }
 
 interface SingleAnswer {
-    answer: QuestionIndex;
+  answer: QuestionIndex;
 }
 
 export interface MultipleAnswer {
-    answers: QuestionIndex[];
+  answers: QuestionIndex[];
 }
 
 export interface ClickableImageAnswer {
-    answer: [number, number];
+  answer: [number, number];
 }
 
-export type Answer = (
-    { ty: AnswerType.Single } & SingleAnswer |
-    { ty: AnswerType.Multiple } & MultipleAnswer |
-    { ty: AnswerType.ClickableImage } & ClickableImageAnswer
-)
+export type Answer =
+  | ({ ty: AnswerType.Single } & SingleAnswer)
+  | ({ ty: AnswerType.Multiple } & MultipleAnswer)
+  | ({ ty: AnswerType.ClickableImage } & ClickableImageAnswer);
