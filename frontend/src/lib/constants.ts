eeefb0323e489a4efd0474a42f7c50583efd81fd
Jacobtread <jacobtread@gmail.com>
2023-05-07 10:16:20 +1200

    Added token validation to connect screen

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -8,6 +8,8 @@ const DEFAULT_MIN_SCORE: number = 10;
 const DEFAULT_MAX_SCORE: number = 100;
 const DEFAULT_BONUS_SCORE: number = 150;
 
+export const TOKEN_LENGTH: number = 5;
+
 export const MIN_ANSWER_TIME: number = 1000; /* 1s */
 export const MAX_ANSWER_TIME: number = 1000 * 60 * 30; /* 30mins */
 
--- frontend/src/lib/pages/Connect.svelte
+++ frontend/src/lib/pages/Connect.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
+  import { TOKEN_LENGTH } from "$lib/constants";
   import * as socket from "$lib/socket";
   import { ClientMessage, ServerMessage } from "$lib/socket/models";
   import { setHome, setJoin } from "$stores/state";
+  import { z } from "zod";
 
-  let token: string = "";
+  let userToken: string = "";
+  let disabled: boolean = true;
+
+  const tokenSchema = z
+    .string()
+    .toUpperCase()
+    .length(TOKEN_LENGTH)
+    .regex(/[A-Z0-9]/);
+
+  function onTokenInput() {
+    userToken = userToken
+      // Convert the value to uppercase format
+      .toUpperCase()
+      // Remove any invald values
+      .replace(/[^A-Z0-9]/, "");
+
+    // Change the disabled state
+    disabled = !tokenSchema.safeParse(userToken).success;
+  }
 
   async function connectQuiz() {
-    // TODO: proper token validation
-    if (token.length < 5) {
+    const parse = tokenSchema.safeParse(userToken);
+
+    if (!parse.success) {
       return;
     }
+
+    const token = parse.data;
 
     // Await the socket being alive
     await socket.ready();
@@ -30,9 +53,15 @@
 <p>Join Game</p>
 <label for="">
   Code
-  <input type="text" bind:value={token} />
+  <input
+    type="text"
+    bind:value={userToken}
+    on:input={onTokenInput}
+    minlength={TOKEN_LENGTH}
+    maxlength={TOKEN_LENGTH}
+  />
 </label>
-<button on:click={connectQuiz}>Connect</button>
+<button on:click={connectQuiz} {disabled}>Connect</button>
 
 <style>
 </style>
