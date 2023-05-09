ba7f3f38f7752071b03e828f3448c61e1cd5a5c3
Jacobtread <jacobtread@gmail.com>
2023-05-10 11:32:11 +1200

    Styled global dialog

--- frontend/src/lib/components/GlobalDialog.svelte
+++ frontend/src/lib/components/GlobalDialog.svelte
@@ -12,20 +12,30 @@
 {#if $dialogStore}
   <div class="dialog-wrapper" transition:fade={{ duration: 200 }}>
     <div class="dialog" transition:slide={{ duration: 200 }}>
-      <h1>{$dialogStore.title}</h1>
-      <p>{$dialogStore.message}</p>
+      <h1 class="dialog__title">{$dialogStore.title}</h1>
+      <p class="dialog__msg">{$dialogStore.message}</p>
 
-      {#if $dialogStore.ty === DialogType.Error}
-        <button on:click={() => consumeDialog(true)}>Close</button>
-      {:else}
-        <button on:click={() => consumeDialog(true)}>Confirm</button>
-        <button on:click={() => consumeDialog(false)}>Cancel</button>
-      {/if}
+      <div class="dialog__actions">
+        {#if $dialogStore.ty === DialogType.Error}
+          <button class="button" on:click={() => consumeDialog(true)}
+            >Close</button
+          >
+        {:else}
+          <button class="button" on:click={() => consumeDialog(true)}
+            >Confirm</button
+          >
+          <button class="button" on:click={() => consumeDialog(false)}
+            >Cancel</button
+          >
+        {/if}
+      </div>
     </div>
   </div>
 {/if}
 
-<style>
+<style lang="scss">
+  @import "../assets/scheme";
+
   .dialog-wrapper {
     position: fixed;
     left: 0;
@@ -36,10 +46,34 @@
     justify-content: center;
     align-items: center;
     background-color: rgba(0, 0, 0, 0.7);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
   }
 
   .dialog {
-    background-color: #111;
+    background-color: $surface;
     padding: 1rem;
+    border-radius: 0.5rem;
+    max-width: 32rem;
+    width: 100%;
+  }
+
+  .button {
+    background-color: $surfaceLight;
+    flex: auto;
+  }
+
+  .dialog__title {
+    margin-bottom: 0.5rem;
+    color: #ffffff;
+  }
+
+  .dialog__msg {
+    margin-bottom: 1rem;
+  }
+
+  .dialog__actions {
+    display: flex;
+    gap: 1rem;
   }
 </style>
