7a69a18ef35163071191244748118e676a6c2ca6
Jacobtread <jacobtread@gmail.com>
2023-05-14 10:30:21 +1200

    Moved github button on home screen and made styling responsive

--- frontend/src/lib/pages/Home.svelte
+++ frontend/src/lib/pages/Home.svelte
@@ -8,57 +8,63 @@
 </script>
 
 <main class="main" transition:slide>
-  <img src={Logo} alt="Quizler Logo" class="logo" width="248" height="135" />
+  <div class="left">
+    <img src={Logo} alt="Quizler Logo" class="logo" width="248" height="135" />
+    <a
+      href="https://github.com/jacobtread/Quizler-v2"
+      target="_blank"
+      rel="noreferrer"
+      title="View on Github"
+      class="btn btn--icon github"
+    >
+      <img src={GitHub} alt="GitHub Icon" />
+      View on GitHub
+    </a>
+  </div>
   <div class="actions">
     <button on:click={setConnect} class="action" aria-label="Join">
       <img src={Play} alt="Join Icon" class="action__icon" />
-      <div>
-        <span class="action__name">Join a quiz</span>
+      <div class="action__body">
+        <p class="action__name">Join a quiz</p>
         <p class="action__text">Enter a game code and hop right in</p>
       </div>
     </button>
 
     <button on:click={setCreate} class="action" aria-label="Create">
       <img src={Edit} alt="Create Icon" class="action__icon" />
-      <div>
-        <span class="action__name">Create a quiz</span>
+      <div class="action__body">
+        <p class="action__name">Create a quiz</p>
         <p class="action__text">Create your own quiz</p>
       </div>
     </button>
   </div>
-
-  <a
-    href="https://github.com/jacobtread/Quizler-v2"
-    target="_blank"
-    rel="noreferrer"
-    title="View on Github"
-    class="github"
-  >
-    <img src={GitHub} alt="GitHub Icon" class="github__icon" />
-  </a>
 </main>
 
 <style lang="scss">
   @use "sass:color";
   @import "../assets/scheme.scss";
 
-  .github {
-    position: absolute;
-    right: 1rem;
-    bottom: 1rem;
-
+  .left {
     display: flex;
     gap: 1rem;
+    flex-flow: column;
+    align-items: center;
+  }
+
+  .github {
+    font-size: 1rem;
+    font-weight: bold;
     text-decoration: none;
-    background: $surface;
+    color: #fff;
     padding: 1rem;
-    border-radius: 2rem;
+    border-radius: 1rem;
   }
 
   .main {
     display: flex;
     justify-content: center;
     align-items: center;
+    overflow: auto;
 
     gap: 3rem;
     height: 100%;
@@ -73,6 +79,7 @@
     display: flex;
     flex-flow: column;
     gap: 1rem;
+    margin: 1rem;
   }
 
   .action {
@@ -143,12 +150,68 @@
   .action__name {
     font-size: 1.4rem;
     margin-bottom: 0.25rem;
-    display: block;
     color: $textPrimary;
   }
 
   .action__text {
     color: $textSecondary;
     font-size: 1.1rem;
+  }
+
+  @media screen and (max-height: 48rem) {
+    .main {
+      justify-content: flex-start;
+      align-items: center;
+      padding: 1rem 0;
+      gap: 1rem;
+    }
+  }
+
+  @media screen and (max-width: 52rem) {
+    .main {
+      flex-flow: column;
+      gap: 1rem;
+    }
+  }
+
+  @media screen and (max-width: 32rem) {
+    .action {
+      flex-flow: column;
+      padding-right: 1rem;
+    }
+
+    .action__icon {
+      width: 100%;
+      max-height: 3rem;
+      padding: 1rem 0;
+    }
+
+    .action__body {
+      display: block;
+      width: 100%;
+    }
+  }
+
+  @media screen and (max-width: 24rem) {
+    .action {
+      flex-flow: column;
+      padding-right: 1rem;
+    }
+  }
+
+  @media screen and (max-width: 16rem) {
+    .action {
+      flex-flow: column;
+      padding-right: 1rem;
+    }
+
+    .action__body {
+      display: none;
+    }
+
+    .action__icon {
+      padding: 1rem;
+      width: auto;
+    }
   }
 </style>
