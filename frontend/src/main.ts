68a5267c17b8fbb45f08996a05329b00f8fce5f5
Jacobtread <jacobtread@gmail.com>
2023-05-16 11:06:09 +1200

    Cast in main to get rid of error

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import "$lib/assets/app.scss";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app")
+  target: document.getElementById("app") as Element
 });
 
 export default app;
