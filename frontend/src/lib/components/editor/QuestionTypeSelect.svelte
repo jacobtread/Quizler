80eaa127079f76e2cfc5dc9b0afeb2876ba47b0c
Jacobtread <jacobtread@gmail.com>
2023-06-16 12:50:21 +1200

    Restyled and improved usability of question type selector

--- frontend/src/lib/components/editor/QuestionTypeSelect.svelte
+++ frontend/src/lib/components/editor/QuestionTypeSelect.svelte
@@ -33,21 +33,53 @@
       Close
     </button>
 
-    <div class="field">
-      <p class="field__name">Question Type</p>
-      <p class="field__desc">The type of question to use</p>
-      <div class="radio">
-        {#each Object.values(QuestionType) as ty}
-          <div>
-            <RadioButton
-              on:change={onTypeChange}
-              bind:group={question.ty}
-              value={ty}
-            >
-              <b>{ty} Choice</b>: {questionTypeText[ty]}
-            </RadioButton>
+    <div class="section">
+      <h2 class="section__title">Question Type</h2>
+      <p class="section__desc">Please select the type of question below</p>
+
+      <div class="types">
+        <label
+          class="type"
+          class:type--selected={question.ty == QuestionType.Single}
+        >
+          <input
+            class="type__input"
+            type="radio"
+            on:change={onTypeChange}
+            bind:group={question.ty}
+            value={QuestionType.Single}
+            tabindex="0"
+          />
+          <p class="type__name">Single Choice</p>
+          <p class="type__desc">Players can only select one answer</p>
+          <div class="answers">
+            <p class="answer answer--correct" />
+            <p class="answer" />
+            <p class="answer" />
+            <p class="answer" />
           </div>
-        {/each}
+        </label>
+
+        <label
+          class="type"
+          class:type--selected={question.ty == QuestionType.Multiple}
+        >
+          <input
+            class="type__input"
+            type="radio"
+            on:change={onTypeChange}
+            bind:group={question.ty}
+            value={QuestionType.Multiple}
+          />
+          <p class="type__name">Multiple Choice</p>
+          <p class="type__desc">Players can select multiple answers</p>
+          <div class="answers">
+            <p class="answer answer--correct" />
+            <p class="answer answer--correct" />
+            <p class="answer" />
+            <p class="answer answer--correct" />
+          </div>
+        </label>
       </div>
     </div>
   </div>
@@ -56,10 +88,93 @@
 <style lang="scss">
   @import "../../../assets/scheme.scss";
 
+  .types {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+  }
+
+  .type {
+    padding: 1rem;
+    border: 2px solid $surfaceLight;
+    border-radius: 0.25rem;
+    transition: border-color 0.25s ease;
+    cursor: pointer;
+
+    &--selected {
+      border-color: $primary;
+    }
+
+    &:hover {
+      border-color: $surfaceLighter;
+    }
+
+    &--selected:hover {
+      border-color: $primaryLighter;
+    }
+
+    &__input {
+      display: none;
+    }
+
+    &__name {
+      font-size: 1.25rem;
+      font-weight: bold;
+      color: #fff;
+      margin-bottom: 0.25rem;
+    }
+
+    &__desc {
+      margin-bottom: 0.5rem;
+    }
+  }
+
+  .answers {
+    overflow: hidden;
+    text-align: center;
+
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    gap: 0.5rem;
+  }
+
+  .answer {
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+    background-color: $surfaceLight;
+    transition: background-color 0.1s linear;
+
+    &:nth-child(odd):last-child {
+      grid-column-start: 1;
+      grid-column-end: 3;
+    }
+
+    &--correct {
+      background-color: $primary;
+      color: #fff;
+    }
+  }
+
   .radio {
     display: flex;
     flex-flow: column;
     gap: 0.5rem;
+  }
+
+  .section {
+    display: flex;
+    flex-flow: column;
+    border: 1px solid #444;
+    padding: 1rem;
+    border-radius: 0.25rem;
+    &__title {
+      color: #ffffff;
+      margin-bottom: 0.25rem;
+    }
+
+    &__desc {
+      margin-bottom: 0.5rem;
+    }
   }
 
   .floating-wrapper {
@@ -96,6 +211,12 @@
   @media screen and (max-width: 48rem), (max-height: 48em) {
     .floating-wrapper {
       align-items: flex-start;
+    }
+  }
+
+  @media screen and (max-width: 36rem) {
+    .types {
+      grid-template-columns: 1fr;
     }
   }
 </style>
