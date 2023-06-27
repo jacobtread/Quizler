ec0fa4297a2c8f90992fae9dba4d23b6229c41b2
Jacobtread <jacobtread@gmail.com>
2023-06-27 12:52:23 +1200

    Made checkboxes accessible to keyboard navigation

--- frontend/src/lib/components/Checkbox.svelte
+++ frontend/src/lib/components/Checkbox.svelte
@@ -37,8 +37,19 @@
       transform: scale(1.1);
     }
 
+    &:focus-within {
+      outline: 2px solid #ffffff;
+    }
+
     &__value {
-      display: none;
+      position: absolute;
+      overflow: hidden;
+      clip: rect(0 0 0 0);
+      height: 1px;
+      width: 1px;
+      margin: -1px;
+      padding: 0;
+      border: 0;
 
       &:checked ~ .checkbox__mark {
         opacity: 1;
