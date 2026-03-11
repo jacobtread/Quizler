b4de0a622fad242a850e970789ab44f643f1d9f6
Jacobtread <jacobtread@gmail.com>
2026-03-11 19:19:24 +1300

    chore: use sveltePreprocess named export

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
-import sveltePreprocess from "svelte-preprocess";
+import { sveltePreprocess } from "svelte-preprocess";
 
 // https://vitejs.dev/config/
 export default defineConfig({
